72b9f360b66b077c9b092cddec0d4b9d2a3a2ba7
Bata22 <nikolic.bratislav123@gmail.com>
2026-09-25 10:15:46 +0200

    Add heap tracker

--- src/MQTT.cpp
+++ src/MQTT.cpp
@@ -33,7 +33,7 @@ void connectMqtt()
 }
 void reconnectMqtt()
 {
-    Serial.printf("[DIAG] MQTT pao : state= %d wifi= %d rssi = %d dBm\n", client.state(), WiFi.status(), WiFi.RSSI());
+    Serial.printf("[DIAG] MQTT pao : state= %d wifi= %d rssi = %d dBm  heap = %d\n", client.state(), WiFi.status(), WiFi.RSSI(), ESP.getFreeHeap());
     while (!client.connected())
     {
         myId = getUniqeNodeId(); // nodeId mac address
